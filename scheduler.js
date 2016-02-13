21985ec9ebb7081886c662b3c219a741871b85c8
Dexter Fichuk <dexterfichuk@gmai.com>
2016-02-13 18:32:10 -0500

    Added FCFS and SJSF

--- scheduler.js
+++ scheduler.js
@@ -2,10 +2,19 @@ console.log('Hello world')
 var aTime=0;
 
 var papa=createArray();
+print(papa);
+
 sortArray(1, papa); //Where first parameter is column to sort by
+fcfs(papa);
+sjsf(papa);
 
 
 
+function print(papa){
+    for (var i = 0; i < papa.length; i++) {
+    console.log(papa[i])
+}
+}
 
 function randomIntInc (low, high) {
     return Math.floor(Math.random() * (high - low + 1) + low);
@@ -17,15 +26,55 @@ function sortArray (sortBy, papaA){
 		//Sort by whatever recieved field
  	   return a[sortBy] - b[sortBy];	
 	});
-	console.log('\nSorted array?')
+	//console.log('\nSorted array?')
 
-for (var i = 0; i < papaA.length; i++) {
-    console.log(papaA[i])
-}
-
+//used to validate sourcing
+//print(papaA);
 return papaA;
 }
 
+function calcEnd(sortedA){
+    for (var i = 0; i < sortedA.length; i++) {
+        if (i==0){
+            sortedA[i][5]=sortedA[i][1];
+        }
+        else    {
+            var j = 5;
+            sortedA[i][5]=sortedA[i-1][j]+sortedA[i][1]+1;
+            //console.log('Reaches else')
+        }
+    }
+    return sortedA;
+}
+
+function fcfs(fcfsA){
+    fcfsA=sortArray(0, fcfsA);
+
+    fcfsA=calcEnd(fcfsA);
+
+    console.log('\nFCFS')
+    print(fcfsA);
+
+    return fcfsA;
+}
+
+function sjsf(sjsfA){
+    sjsfA=sortArray(1, sjsfA);
+
+    sjsfA=calcEnd(sjsfA);
+
+    console.log('\nSJSF')
+
+    sjsfA=sortArray(0, sjsfA);
+
+    print(sjsfA);
+
+    return sjsfA;
+}
+
+//
+//  Function to create our random array
+//
 function createArray(){
 	console.log('Runtimes:')
 var runTime = new Array(10);
@@ -61,7 +110,7 @@ for (var i = 0; i < stride.length; i++) {
 console.log('\nPapa Array:')
 var papa = new Array(10);
 var titles = new Array(10);
-titles = ['Job#','Run Time', 'Arrival Time', 'Tickets', 'Stride']
+titles = ['Job#','Run Time', 'Arrival Time', 'Tickets', 'Stride', 'End Time']
 
 var caption='';
 
@@ -72,8 +121,7 @@ console.log(caption)
 
 
 for (var i = 0; i < papa.length; i++) {
-    papa[i]=['000'+i,runTime[i], arrivalTime[i], tickets[i], stride[i]];    
-    console.log(papa[i])
+    papa[i]=['000'+i,runTime[i], arrivalTime[i], tickets[i], stride[i], 0];
 }
 return papa;
 }
