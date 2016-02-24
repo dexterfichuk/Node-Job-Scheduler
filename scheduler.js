634bff905e65884ebfb5ce47dd2c07dc30af2fc2
dexterfichuk <dexterfichuk@gmail.com>
2016-02-24 00:20:11 -0500

    COMMENTS

--- scheduler.js
+++ scheduler.js
@@ -1,16 +1,40 @@
-global.fcount=0;
+
+/*============================
+||	Dexter Fichuk
+||	Assignment 1
+||	COIS-3320H
+||	0552537
+||	Pleaase enjoy my irrational Logic :')
+||	***********************************************************************************************************
+||	NOT COMPLETELY COMMENTED, to see rest of comments (sorry if this is cheating) but check it out on GitHub)
+||	https://github.com/dexterfichuk/Node-Job-Scheduler (don't worry, was a private repository until assignment submitted!)
+
+==============================*/
 
 //Make output directory if it doesn't exist
 var fs = require('fs');
 if (!fs.existsSync('./output')){
     fs.mkdirSync('./output');
 }
+
+//Array to be used for appending turnaround and wait times
 var calculatedA= new Array();
 
-for (var fcount = 0; fcount < 20; fcount++) {
-    var papa=createArray();
-    var papajr=papa.slice(0);
+//Variable for job loop, and used for file number when writing to csv's
+var fcount=0;
 
+//For loop to choose number of times to run the jobs. (20 for this assignment, but why not try 100000?)
+for (var fcount = 0; fcount < 20; fcount++) {
+
+	//Create generated array named papa
+    var papa=createArray();
+
+    //Had some problems with using the same round robin function more than once in my loop with the same array so had to make copies to pass in
+    var papajr=papa.slice(0);
+    var papasr=papa.slice(0);
+    var papaps=papa.slice(0);
+
+    //Print the original generated array
     print(papa);
 
     sortArray(1, papa); //Where first parameter is column to sort by
@@ -21,68 +45,88 @@ for (var fcount = 0; fcount < 20; fcount++) {
     // arrayWriter('Stride', stride(5, papa));
     // arrayWriter('SJF', sjf(papa));
     // arrayWriter('Round Robin', roundRobin(papa));
-    // arrayWriter('PSJF', psjf(papajr));
+    // arrayWriter('STCF', stcf(papajr));
 
+    //Run all the schedulers, then calculate the turnaround and wait times, then append it to the calculated array to be printed to a csv with all results
     calculatedA=calculator('FCFS', fcfs(papa), calculatedA);
-
     calculatedA=calculator('Stride', stride(5, papa), calculatedA);
-
     calculatedA=calculator('SJF', sjf(papa), calculatedA);
+    calculatedA=calculator('Round Robin 05', roundRobin(5, papa), calculatedA);
+    calculatedA=calculator('Round Robin 10', roundRobin(10, papajr), calculatedA);
+    calculatedA=calculator('Round Robin 15', roundRobin(15, papasr), calculatedA);
+    calculatedA=calculator('STCF', stcf(papaps), calculatedA);
 
-    calculatedA=calculator('Round Robin 5', roundRobin(5, papa), calculatedA);
-
-    calculatedA=calculator('Round Robin 10', roundRobin(10, papa), calculatedA);
-
-    calculatedA=calculator('Round Robin 15', roundRobin(15, papa), calculatedA);
-
-    calculatedA=calculator('PSJF', psjf(papajr), calculatedA);
-
+    //Truncate the base array just for safety measures
     papa.length=0;
 }
 
+//For the sake of writing the log of all averages, reduce fcount so file name reflects how many times it actually run through, otherwise for 5 jobs file number would say 6
 fcount-=1;
 
+//Use my favorite overused sorting algorithm to sort the array by job number
 calculatedA = sortArray(1, calculatedA);
 
+//Add the headings to the array
 calculatedA.unshift(['Scheduler', 'Round #', 'Avg Turnaround', 'Avg Wait']);
 
+//Write the averages log to a csv
 arrayWriter('CalculatedTotals', calculatedA);
 
 
-
+//
+//	Will calculate the average runtime and wait time for the passed in array, then append it to the master average log, and will add the name of the scheduler to the first column
+//
 function calculator(schedulerName, scheduledArray, calculatedArray){
     //avgTurnaround = (endtime - arrivaltime)/numJobs
     //avgWaiting = (startTime-arrivalTime)/numjobs
     var avgTurnaround=0, avgWaiting=0, turnSum=0, waitSum=0;
 
+    //Calculate the sums of the turnarounds and waits by looping through entire array.
     for (var i=0; i < scheduledArray.length; i++){
         turnSum+=scheduledArray[i][6]-scheduledArray[i][2];
         waitSum+=(scheduledArray[i][5]-scheduledArray[i][2]);
     }
+
+    //Now divide the sums by the length of the array to get the average
     avgTurnaround=turnSum/scheduledArray.length;
     avgWaiting=waitSum/scheduledArray.length;
 
+    //Print the averages for a little fun validation
     console.log('Avg Waiting = '+avgWaiting);
+    console.log('Avg Turnaround = '+avgTurnaround);
 
+    //Make an array of the averages and return it
     calculatedArray[calculatedArray.length]=[schedulerName, fcount, avgTurnaround, avgWaiting];
-
     return sortArray(1, calculatedArray);
 }
 
+//
+//	Print Function: Used for looping through multidimesnional arrays and printing them out.
+//
 function print(papa){
+	//Header of jobs, couldn't be bothered to make it all format output nicely, sorry Jacques
     var heade = ['Job#','Run Time', 'Arrival Time', 'Tickets', 'Stride', 'Start Time', 'End Time', 'Pass', 'Time Left']
 
+    //Convert header to string and print
     console.log(heade.toString());
 
+    //Loop through arrays and print elements.
     for (var i = 0; i < papa.length; i++) {
     console.log(papa[i])
     }
 }
 
+//
+//	Function to generate and random integer between the low and high values (including them)
+//
 function randomIntInc (low, high) {
+	//Rounds the value, then returns it
     return Math.floor(Math.random() * (high - low + 1) + low);
 }
 
+//
+//	Sorts the array by the index passed into it for sortBy
+//
 function sortArray (sortBy, papaA){
 	papaA.sort(function(a, b)
 	{
@@ -96,6 +140,9 @@ function sortArray (sortBy, papaA){
 return papaA;
 }
 
+//
+//	Will calculate the end times of the array, mostly for FCFS
+//
 function calcEnd(sortedA){
     for (var i = 0; i < sortedA.length; i++) {
         if (i==0){
@@ -112,13 +159,16 @@ function calcEnd(sortedA){
     return sortedA;
 }
 
+//
+//	Will perform the round robing algorithm on the passed in array with whatever chunk size was passed in
+//
 function roundRobin( chunkS, papa){
+	//
+	//truncate and make copies of arrays to ignore weird global problems, and make some other variables
     var baseA = new Array(0);
     baseA.length=0;
     baseA = papa.splice(0);
     var finalA = new Array (0);
-    //var quant = [15, 15, 15];
-    //var qSize = 0;
     var clock = 0;
 
     //Set time left to run time and a few non-important fields to 0
@@ -129,57 +179,74 @@ function roundRobin( chunkS, papa){
         baseA[i][6]=0;
     }
 
+    //While loop to run until the original array is completely empty
     do {
-
+    	//Run through array elements in loop until i=the arrays size (will change over the process)
         for (var i=0; i < baseA.length; i++){
 
+        	//If arrival time is greater than clock, then set clock to arrival time without overhead.
             if (baseA[i][2]>clock){
                 clock=baseA[i][2]-1;
             }
 
+            //Remove the time chunk from the time left of the i'th element
             baseA[i][8]-=chunkS;
 
+
+            //Check to if start time is zero for current job, then set it to clock, but never for job #0000 as it will always start first
             if (baseA[i][5]==0 && baseA[i][0]!='0000'){
                 baseA[i][5]=clock+1;
             }
 
+            //If time leftt = 0 (job is completed)
             if (baseA[i][8]==0){
+
+            	//Increment clock by chunk size
                 clock+=chunkS;
+
+                //Set end time to clock
                 baseA[i][6]=clock;
+
+                //Add completed job to finished array then splice away <3
                 finalA[finalA.length]=baseA[i];
                 baseA.splice(i, 1);
+
+                //Remove 1 from i so a value isn't skipped in the for loop. Will set the index back for when it increments so the row that was moved lower in array will be used next.
                 i--;
             }
+
+            //Almost same as previous if, except for if a job finished early
             else if (baseA[i][8]<0)
             {
+            	//Clock increments by the time used since the job finished early
                 clock+=chunkS+baseA[i][8];
+
+                //Set time left to 0 for looks. Rest is same as before
                 baseA[i][8]=0;
                 baseA[i][6]=clock+1;
                 finalA[finalA.length]=baseA[i];
                 baseA.splice(i, 1);
                 i--;
             }
+
+            //If job hasn't completed, continue to increase clock by chunk size and go on your merry way
             else {
                 clock+=chunkS;
             }
         }
 
-        // if (qSize==2){
-        //     qSize=0;
-        // }
-        // else {
-        //     qSize++;
-        // }
-
+    //Loop until original array is empty
     } while (baseA.length!=0);
 
+    //Sort, print and return finished array
     console.log('Round Robin');
     finalA=sortArray(0,finalA);
     print(finalA);
-
     return finalA;
 
     /*
+    var quant = [15, 15, 15];
+    qSize = 0;
     //Do until the baseA array is empty (finished processing)
     do {
         //Deduct current quant size from time left
@@ -254,6 +321,7 @@ function stride(cpuTime, pArray){
     var papa = pArray.slice(0);
     var q=0;
 
+    //Set start time to 0 for all
     for (var i=0; i < pArray.length; i++){
         pArray[i][5]=0;
     }
@@ -454,7 +522,7 @@ function sjf(papa){
 }
 
 //Shotest Job First
-function psjf(arr){
+function stcf(arr){
     //Sorts the array by arrival times, redudent
     var originalA = new Array(arr.length);
     originalA=sortArray(2, arr.slice(0));
@@ -538,18 +606,18 @@ function psjf(arr){
             // console.log('\nArrived Array');
             // print(arrivedArray);
 
-            //console.log('\nPSJF Array');
+            //console.log('\nSTCF Array');
             //print(arrivedArray);
 
         
     } while (arrivedArray.length!=0);
 
-    console.log('\nPSJF Array');
+    console.log('\nSTCF Array');
     print(sortArray(0, completeA));
     return completeA;
 }
 
-//Adds newArray to oldArray
+//Adds newArray to oldArray, not really used at all
 function appendArray(oldArray, newArray){
     do{
         oldArray[oldArray.length]=newArray[0];
@@ -567,6 +635,8 @@ function calcSEnd(tempA){
     return tempA;
 }
 
+//
+//	is actually not used.
 function stcf(stcfA){
     stcfA=sortArray(1, stcfA);
 
