b449e6be0f7c578953e3111da5f19dc78bccb6b6
Dexter Fichuk <dexterfichuk@gmai.com>
2016-02-25 10:05:33 -0500

    broken

--- scheduler.js
+++ scheduler.js
@@ -244,6 +244,7 @@ function roundRobin( chunkS, papa){
     print(finalA);
     return finalA;
 
+    //And old abandoned array I made when I misunderstood round robin scheduling, one day I will revive this and make it a new amazing scheduler
     /*
     var quant = [15, 15, 15];
     qSize = 0;
@@ -396,30 +397,26 @@ function stride(cpuTime, pArray){
         
     } while (papa.length!=0);
 
+    //Sort, then print the complete array, then return it.
     console.log(papa);
     stridedArray=sortArray(0,stridedArray);
-
     console.log('\nStrided Array:');
     print(stridedArray);
 
     return stridedArray;
 }
 
-
+//First come first serve algorithm
 function fcfs(fcfsA){
+	//Sort by job number ascending
     fcfsA=sortArray(0, fcfsA);
 
+    //Calculate end times for all
     fcfsA=calcEnd(fcfsA);
 
-    // for (var i=0; i < fcfsA.length; i++){
-    //     fcfsA[i][5]=0;
-    // }
-
+    //Print and return the array.
     console.log('\nFCFS')
     print(fcfsA);
-
-    arrayWriter("FCFS",fcfsA);
-
     return fcfsA;
 }
 
@@ -521,9 +518,9 @@ function sjf(papa){
     return hArray;
 }
 
-//Shotest Job First
+//Shortest Job First Algorithm
 function stcf(arr){
-    //Sorts the array by arrival times, redudent
+    //Sorts the array by arrival times, redudent, and makes a copy of parameter array
     var originalA = new Array(arr.length);
     originalA=sortArray(2, arr.slice(0));
     //print(originalA);
@@ -532,11 +529,15 @@ function stcf(arr){
     //console.log('Loop Count: '+loopCounter);
     loopCounter++;
 
+    //Clear some of the columns and set the time left to the run time
     for (var i=0; i < originalA.length; i++){
         originalA[i][8]=originalA[i][1];
         originalA[i][6]=0;
         originalA[i][5]=0;
     }
+
+    //This was extremely weird, but I could only get my loop working but it would not process the very last row with the highest arrival time, 
+    //so my way of fixing it was to auto add a row to the very end with a arrival time +1 higher than the highest.
     originalA[originalA.length]=[,,originalA[originalA.length-1][2]+1,,,,];
 
     //print(originalA);
@@ -546,72 +547,75 @@ function stcf(arr){
     var tcounter=0;
     var compareHolder = new Array();
     
+    //Comparison used to see if the previously processed array is now different from the current shorters job
     compareHolder.push(originalA[0]);
-    //arrivedArray[0]=originalA.shift();
 
     console.log(arrivedArray[0]);
-    //originalA.splice(0, 1);
 
+    //while loop that runs until the arrived array has been emptied (every item has been processed)
     do{
-
+    	//Set arrived array to be itself but sorted by run times
         arrivedArray=sortArray(1, arrivedArray);
 
-
+        //Increment clock overhear
         clock+=1;
 
+        //if the compare holder array is empty, sets it's job number to the first in the arrived array, mostly used for first arrival
         if (compareHolder.length==0){
             compareHolder[0][0]=arrivedArray[0][0];
         }
 
+        //while loop that quits when flag is set to 1 (quitDo)
         do {
+        	//Flag for breaking loop
             var quitDo=0;
+
+            //If the start time is less than the clock the the original array has mroe than 1 item
             if (originalA[0][2] < clock && originalA.length!=1){
 
+            	//If the start time = 0, set it to the clock -the overhead (1)
                 if (originalA[0][5]==0);{
                     originalA[0][5]=clock-1;
                 }
+                //Put the item that has arrived into the arrivaed array.
                 arrivedArray[arrivedArray.length]=originalA[0];
+
+                //Remove it from original
                 originalA.splice(0, 1);
             }
-            // else if (originalA.length==1){
-            //     arrivedArray[arrivedArray.length]=originalA[0];
-            //     originalA.splice(0, 1);
-            // }
+            //Otherwise quit this process and continue the next loop
             else {
                 quitDo=1;
             }
             } while (quitDo==0);
 
-        //console.log('out of do1');
+        //Sort arrived array by the runtimes
         arrivedArray=sortArray(1, arrivedArray);
 
+        //Check if the shortest job in arrived array is the same as last time, if not, increment clock by the overhead to switch
         if (arrivedArray[0][0]!=compareHolder[0][0]){
             clock+=1;
         }
 
+        //Add sme overhead
         clock+=1;
-        arrivedArray[0][8]-=1;
-        //console.log('Arrived array clock at '+clock);
-        //print(arrivedArray);
 
+        //Reduce the time left of the shortest job by 1
+        arrivedArray[0][8]-=1;
+
+        //If the time left of the shortest job = 0
         if (arrivedArray[0][8]==0){
+        	//Set the end time to the clock
             arrivedArray[0][6]=clock;
+
+            //Add the row to the completed array and remove it from arrived
             completeA[completeA.length]=arrivedArray[0];
             arrivedArray.splice(0, 1);
         }
-
-            // console.log('\nBase Array');
-            // print(originalA);
-
-            // console.log('\nArrived Array');
-            // print(arrivedArray);
-
-            //console.log('\nSTCF Array');
-            //print(arrivedArray);
-
-        
+    //Quit when arrivedArray finished processing        
     } while (arrivedArray.length!=0);
 
+    //Sort and return complete array
     console.log('\nSTCF Array');
     print(sortArray(0, completeA));
     return completeA;
@@ -629,14 +633,14 @@ function appendArray(oldArray, newArray){
     return oldArray;
 }
 
+//Calcs the end time of the last object in the array
 function calcSEnd(tempA){
-    //console.log(tempA[tempA.length-1][6]);
     tempA[tempA.length-1][6]=tempA[tempA.length-2][6]+(tempA[tempA.length-1][1])+1;
     return tempA;
 }
 
 //
-//	is actually not used.
+//	*******is actually not used. some old code just left here for reference purposes
 function stcf(stcfA){
     stcfA=sortArray(1, stcfA);
 
@@ -649,15 +653,21 @@ function stcf(stcfA){
     print(stcfA);
 
     return stcfA;
-}
 
+//
+//	Array writer, capable of writing any multi-dimension matrix to a csv file
+//	//shedulerName=Name you want at end of file // papa=Passed matrix
 function arrayWriter(schedulerName, papa){
+
+	//Set file to null
     var file='';
+
+    //loop through arrays adding commas between and appending it to the file var
     for (var i = 0; i < papa.length; i++) {
     file+= papa[i].toString()+',\n';
     }
-    //console.log(file);
 
+    //Write the file, and retrieve the file number for name from global fcount variables (used in for loop at beginning)
     var fs = require('fs');
     fs.writeFile("output/file"+schedulerName+(fcount+1)+".csv", file, function(err) {
     if(err) {
@@ -670,10 +680,11 @@ function arrayWriter(schedulerName, papa){
 }
 
 //
-//  Function to create our random array
+//  Function to create our random array with all of our beutifully generated values
 //
 function createArray(){
 	//console.log('Runtimes:')
+//Creates runtimes between 2 and 60 and puts in a array 10 times
 var runTime = new Array(10);
 for (var i = 0; i < runTime.length; i++) {
     runTime[i] = randomIntInc(2,60)
@@ -681,26 +692,31 @@ for (var i = 0; i < runTime.length; i++) {
 }
 
 //console.log('\nArrival Times:')
+//Generates arrival times incremented by a random value between 5 and 8, then adds them to an array 10 times
 var arrivalTime = new Array(10);
 var aTime=0;
 arrivalTime[0]=0;
 for (var i = 1; i < arrivalTime.length; i++) {
-	aTime+=randomIntInc(5,8);
-    arrivalTime[i] = aTime;
+	aTime+=randomIntInc(5,8);//Increment the times
+    arrivalTime[i] = aTime; 
     //console.log(arrivalTime[i])
 
 }
 
 //console.log('\nTickets:')
+//Generates tickets of either 50, 100, 150 or 200
 var tickets = new Array(10);
 for (var i = 0; i < tickets.length; i++) {
+	//generate number between 1 and 4, then multiply by 50
     tickets[i] = (randomIntInc(1,4)*50);
     //console.log(tickets[i])
 }
 
 //console.log('\nStride:')
+//Calculates strides based on tickets and adds them to an array
 var stride = new Array(10);
 for (var i = 0; i < stride.length; i++) {
+	//1000 is our ticket account to be used
     stride[i] = Math.ceil(1000/tickets[i]);
     //console.log(stride[i])
 }
@@ -708,18 +724,20 @@ for (var i = 0; i < stride.length; i++) {
 console.log('\nPapa Array:')
 var papa = new Array(10);
 var titles = new Array(10);
-titles = ['Job#','Run Time', 'Arrival Time', 'Tickets', 'Stride', 'Start Time', 'End Time', 'Pass']
+//Puts the column headings is an array for reference
+titles = ['Job# 0','Run Time 1', 'Arrival Time 2', 'Tickets 3', 'Stride 4', 'Start Time 5', 'End Time 6', 'Pass 7']
 
 var caption='';
 
+//Loop and put titles into a nice string seperated by pipelines
 for (var i = 0; i < titles.length; i++) {
-    caption += (titles[i]+' | ')
+    caption += (titles[i]+' | ');
 }
 console.log(caption)
 
-
+//Create our master, multi dimensional array by looping through the values of all arrays as i and placing them into rows
 for (var i = 0; i < papa.length; i++) {
-    papa[i]=['000'+i,runTime[i], arrivalTime[i], tickets[i], stride[i], 0, 0, 0, runTime[i]];}
-
+    papa[i]=['000'+i,runTime[i], arrivalTime[i], tickets[i], stride[i], 0, 0, 0, runTime[i]];
+}
 return papa;
 }
